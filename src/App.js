a34533985bb09bf26d3483f74317173ad44c1218
lluisllc <lluis.llacera@gmail.com>
2022-07-13 19:56:18 +0200

    Iteración 3

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 // src/App.js
 import './App.css';
 import foods from './foods.json';
+import FoodBox from './components/FoodBox';
 import { useState } from "react";
-import {Divider, Card, Col, Button} from 'antd';
-export default App;
+// eslint-disable-next-line no-unused-vars
+import { Divider } from 'antd';
 
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
   const [foodList, setFoodList] = useState(foods);
-
-
+  console.log(foodList)
   return <div className="App">
 
-<Divider>Food List</Divider>
-{foodList.map((food) => (
-<div>
-  <p>{food.name}</p>
-  <img src={food.image} width={200} />
-</div>
-
-))}
-</div>;
+    <h1>Food List</h1>
+    {foodList.map((food,index) => {
+      return (<FoodBox className="box"  key={index} food={food} />);
+    })
+    }
+  </div>
 }
+
+export default App;
 
 
 
